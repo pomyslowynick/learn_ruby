173c903b254fdad7f8502798e56954f453bc8c33
Rafal Rosa <dark0mlet@gmail.com>
2017-01-15 18:45:05 +0000

    Third commit, doin good today

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -15,3 +15,29 @@ def sum arr
       return arr[0]
     end
 end
+def multiply arr
+  return arr.reduce(:*)
+end
+def power(num1, num2)
+    return num1**num2
+    
+end
+def factorial num
+  var = num
+  sum = 0
+  if num == 1
+      sum = 1
+  else
+    while var > 1
+      if sum == 0
+        sum += var * (var - 1)
+        var -= 2
+      else
+          var -= 1
+          sum += sum*var
+      end
+    end
+  end
+  return sum
+end
+print factorial(3)
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,52 @@
-#write your code here
+def echo word
+    return word.to_s
+end
+def shout word
+    return word.to_s.upcase
+end
+def repeat(word, *p) 
+    if p.empty?
+      return word.to_s + " " + word.to_s
+    else
+        ret = []
+        i = p.join.to_i
+        while i > 0
+          ret.push(word.to_s)
+          i -= 1
+        end
+        return ret.join(" ")
+    end
+end
+
+def start_of_word(word, let)
+   us = word.split("")
+   if let > 1
+      return us[0..let-1].join 
+   else
+      return us[0]
+   end
+end
+
+def first_word word
+   spli = word.split
+   return spli[0]
+end
+
+def titleize word
+    lilwordz = ["and", "over", "the", "a"]
+    res = ""
+    spli = word.split
+    i = 0
+    while i < spli.length
+      if lilwordz.include? spli[i] and i != 0
+        res += spli[i].to_s + " "
+        i += 1
+      else
+        res += spli[i].to_s.capitalize + " "
+        i += 1
+      end
+    end
+    return res.split(" ").join(" ")
+    
+end
+print titleize("the Bridge over the River Kwai")
