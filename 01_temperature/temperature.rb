f572d6d65aa26590ca4ac4344f3e0eb54b478029
Rafal Rosa <dark0mlet@gmail.com>
2017-01-15 14:39:28 +0000

    Second commit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc fahrenheit
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+end
+def ctof cels2
+    fahrenheit = cels2 * 9.0/5.0 + 32
+    return fahrenheit
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add (num1, num2)
+    return num1 + num2
+end
+def subtract(num1, num2)
+    return num1 - num2
+end
+def sum arr 
+    if arr == []
+        return 0
+    elsif arr.length == 2
+      return arr[1] + arr[0]
+    elsif arr.length > 2
+      arr.inject(0){|sum,x| sum + x }
+    else
+      return arr[0]
+    end
+end
